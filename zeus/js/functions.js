781eebf202ab168b9bb7ed97be4137c426d46db4
Joan Leon <joan.leon@gmail.com>
2016-03-21 12:29:31 +0100

    Refactor syntax

--- zeus/js/functions.js
+++ zeus/js/functions.js
@@ -1,23 +1,25 @@
- /* Funciones */
-  function resizeIframe(iframeW,input){
-    $('#iframe, .sizes').animate({width:iframeW}, 500);
-    if(input == 1){
-          var inputResolution = $('#input-resolution');
-          inputResolution.prop('Counter',$('#iframe').width() + 41).animate({
-              Counter: iframeW
-          }, {
-              duration: 500,
-              easing: 'swing',
-              step: function (now) {
-                  inputResolution.val(Math.ceil(now) + 'px');
-              }
-          });
-    }
-  };
+ /* Functions */
+ function resizeIframe(iframeW, input) {
+   $('#iframe, .sizes').animate({
+     width: iframeW
+   }, 500);
+   if (input == 1) {
+     var inputResolution = $('#input-resolution');
+     inputResolution.prop('Counter', $('#iframe').width() + 41).animate({
+       Counter: iframeW
+     }, {
+       duration: 500,
+       easing: 'swing',
+       step: function(now) {
+         inputResolution.val(Math.ceil(now) + 'px');
+       }
+     });
+   }
+ };
 
-  function copyToClipboard(text) {
-    $(".codebox").select();
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'correctamente' : 'incorrectamente';
-    alert('Copiado ' + msg);
-  }
+ function copyToClipboard(text) {
+   $(".codebox").select();
+   var successful = document.execCommand('copy');
+   var msg = successful ? 'correctamente' : 'incorrectamente';
+   alert('Copiado ' + msg);
+ }
--- zeus/js/index.js
+++ zeus/js/index.js
@@ -5,22 +5,22 @@ $(function() {
   var menuFloat = (initW - 990) / 2 - 190;
   var backTop = (initW - 990) / 2 - 84;
 
-  /* Resize Iframe */ 
+  /* Resize Iframe */
   $('.sizes ul li').click(function() {
     var iframeW = parseInt($(this).attr('data-size'), 10);
     $('.sizes ul li.active').removeClass('active');
     $(this).addClass('active');
-    resizeIframe(iframeW,1);
+    resizeIframe(iframeW, 1);
   });
 
   $('#input-resolution').on("change paste", function() {
     var vpWidth = $(this).val();
-    resizeIframe(vpWidth,0);
+    resizeIframe(vpWidth, 0);
   });
 
   /* Menu Float */
-  $(".menu-float").css('right',menuFloat);
-  $(".button-arrow").css('right',backTop);
+  $(".menu-float").css('right', menuFloat);
+  $(".button-arrow").css('right', backTop);
 
   /* Copy text */
   $("#copy-content").click(function() {
@@ -28,14 +28,16 @@ $(function() {
   });
 
   $('#picto-img').click(function() {
-    $( ".menu-dark" ).slideToggle(300);
-      $(this).toggleClass('rotated-left rotated-right');
+    $(".menu-dark").slideToggle(300);
+    $(this).toggleClass('rotated-left rotated-right');
   });
 
   $('.button-arrow').click(function(event) {
-      event.preventDefault();
-      $('html, body').animate({scrollTop: 0}, duration);
-      return false;
+    event.preventDefault();
+    $('html, body').animate({
+      scrollTop: 0
+    }, duration);
+    return false;
   })
 
 });
